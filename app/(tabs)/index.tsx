16a2537e97dcac8b56452d7d2ceaf276e979a384
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2025-02-17 22:47:17 +0100

    feat: added button component, imageViewer component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Buttons";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -7,7 +8,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" />
+          <Button label="Use this photo" />
+        </View>
       </View>
     </View>
   );
@@ -26,5 +31,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
