f8c9fac178a0c4c3e6df13693e8294e8ce1c1366
mnbd <mnbdpro@gmail.com>
2025-02-17 16:32:19 +0100

    chore: first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Regles
+
+## Installation
+
+```bash
+npx create-expo-app@latest
+```
+
+## Lancer le projet
+
+```bash
+npx expo start
+```
+
+Pour lancer en mode tunnel:
+
+```bash
+npx expo start --tunnel
+```
+
+## Assets
+
+Telecharger les assets sur Teams et les placer dans le dossier `assets` du projet.
+
+## Reinitialiser le projet
+
+```bash
+npm run reset-project
+```
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={styles.button}>
+        Allez à l'écran à propos.
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
